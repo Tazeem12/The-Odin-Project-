4f7036caad9d64b4f67059db8ec2939d7ff643f4
Tazeem <CodingJourney786@gmail.com>
2026-01-14 18:35:58 +0000

    just practising functions code is  not quite accurate

---
+++ function-basic-practice/index.js
@@ -0,0 +1,19 @@
+
+      /*function add7(x){
+        return x+7;
+      };*/
+     /* const multiply= function(a,b){
+      return a*b;
+      };*/
+      var str ="hello";
+      let lower=str.toLowerCase() ;
+      let answer=lower[0].toUpperCase() + lower.slice(1); 
+      function capitalize(str){
+        return answer;
+       }
+      //let capitalize2= (ABCD) => Abcd;
+      //let capitalize3= (aBcD) => Abcd;
+      //console.log(add7(10));
+     // console.log(multiply(10,10));     
+      console.log(capitalize("tazeEm"));
+      //,capitalize2(TAJJ),capitalize3(tAjJ));
