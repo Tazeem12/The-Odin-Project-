57ac4d5139fd713e675a656119fcf64a3f4e4557
Tazeem <CodingJourney786@gmail.com>
2026-01-16 14:47:10 +0000

    including prompt sync

--- Rock-Paper-Scissors/rock-paper-scissors.js
+++
@@ -1,4 +0,0 @@
-var prompt=require('prompt-sync')();
-
-const name=prompt("Enter the value:");
-const age=prompt("Enter the age:");
---
+++ Rock-Paper-Scissors/script.js
@@ -0,0 +1,6 @@
+const prompt=require('prompt-sync')();
+
+const name=prompt("Enter the value:");
+const age=prompt("Enter the age:");
+
+console.log('Hello ${name},you are ${age}')
