a78043edd33a02ea6febfedd8c40c8dfd55ad16e
Tazeem <CodingJourney786@gmail.com>
2026-01-18 05:28:49 +0000

    working on the playround function

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -1,6 +1,54 @@
 const prompt=require('prompt-sync')();
 
-const name=prompt("Enter the value:");
-const age=prompt("Enter the age:");
+function getComputerChoice(){
+    let choice=Math.floor(Math.random()*3);
+    if(choice==0){
+        return "rock";
+    }
+    else if(choice==1){
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
+}
+//console.log("computer choice:",getComputerChoice());
 
-console.log('Hello ${name},you are ${age}')
+function getHumanChoice(){
+    let input=prompt("Enter your choice: ");
+    if((input=="rock")||
+    (input=="paper")||
+    (input=="scissors")){
+        return input;
+    }
+    else{
+        console.log("Please enter a valid choice:rock,paper,scissors.")
+    }
+}
+console.log("computer choice:",getComputerChoice());
+
+var humanScore=0;
+var computerScore=0;
+function playRound(computerSelection,humanSelection){
+    if(computerSelection==humanSelection){
+        console.log("Its a Tie");
+    }
+    else if((humanSelection=="scissors" && computerSelection=="rock")||
+(humanSelection=="rock" && computerSelection=="paper")||
+(humanSelection=="paper" && computerSelection=="scissors")
+){
+    computerScore++;
+    console.log("You Lose");
+}
+else if((humanSelection=="rock" && computerSelection=="scissors")||
+(humanSelection=="paper" && computerSelection=="rock")||
+(humanSelection=="scissors" && computerSelection=="paper")){
+       humanScore++;
+       console.log("You Wins");
+}
+}
+let humanInput=getHumanChoice();
+let computerInput=getComputerChoice();
+
+playRound(computerInput,humanInput);
+console.log(computerScore,humanScore);
